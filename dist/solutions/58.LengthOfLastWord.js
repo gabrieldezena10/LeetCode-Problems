861885af540fa10ea44a2cefef3d757968a78309
gabrieldezena10 <gabriel.deztor@gmail.com>
2023-03-31 15:54:33 -0300

    feat: implement LengthOfLastWord solution

---
+++ dist/solutions/58.LengthOfLastWord.js
@@ -0,0 +1,33 @@
+"use strict";
+/*
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+*/
+function lengthOfLastWord(s) {
+    const lastWord = s.trim().split(/\s+/).slice(-1);
+    return lastWord.toString().length;
+}
+;
+console.log(lengthOfLastWord("   Hello        Worlds  ")); //6
